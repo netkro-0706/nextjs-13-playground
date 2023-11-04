6d6d7a7a0ab33d8fd10a0268192396ef1c02cfc2
netkro <kroging@gmail.com>
2023-11-04 21:32:10 +0900

    add: test page

---
+++ app/abc/page.js
@@ -0,0 +1,13 @@
+export default function Abc() {
+  return (
+    <div>
+      <h4 className="title">abc</h4>
+      <div className="food">
+        <h4>상품1 $40</h4>
+      </div>
+      <div className="food">
+        <h4>상품2 $40</h4>
+      </div>
+    </div>
+  );
+}
