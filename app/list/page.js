703efb1262d5cdcf292ec0b52b453fe46b6fb69f
netkro <kroging@gmail.com>
2023-11-04 22:32:49 +0900

    add: add img in list page

--- app/list/page.js
+++ app/list/page.js
@@ -1,13 +1,17 @@
 export default function List() {
+  let items = ["Tomatoes", "Pasta", "Coconut"];
+
   return (
     <div>
       <h4 className="title">상품목록</h4>
-      <div className="food">
-        <h4>상품1 $40</h4>
-      </div>
-      <div className="food">
-        <h4>상품2 $40</h4>
-      </div>
+      {items.map((item, i) => {
+        return (
+          <div className="food" key={i}>
+            <img src={`/food${i}.png`} className="food-img" alt="food" />
+            <h4>{item} $40</h4>
+          </div>
+        );
+      })}
     </div>
   );
 }
