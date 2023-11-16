742dfb62b408385f0a58221efbe351ef0c8788a9
netkro <kroging@gmail.com>
2023-11-17 02:03:01 +0900

    async test

--- app/components/templates/test.js
+++ app/components/templates/test.js
@@ -37,6 +37,7 @@ export default function Test() {
     return new Promise((res, rej) => {
       setTimeout(() => {
         res("2번 주문됨");
+        //rej(new Error("...is error"));
       }, 3000);
     });
   };
@@ -49,14 +50,32 @@ export default function Test() {
     });
   };
 
+  //async
+  async function order() {
+    console.log("시작");
+    try {
+      const result = await Promise.all([f1(), f2(), f3()]);
+      // const result1 = await f1();
+      // const result2 = await f2(result1);
+      // const result3 = await f3(result2);
+      console.log(result);
+    } catch (e) {
+      console.log(e);
+    }
+    console.log("종료");
+  }
+  order();
+
   //Promise race
-  console.time("x");
-  Promise.race([f1(), f2(), f3()]).then((res) => {
-    console.log(res);
-    console.timeEnd("x");
-  });
+  //가장 먼저 완료된 결과를 반환
+  // console.time("x");
+  // Promise.race([f1(), f2(), f3()]).then((res) => {
+  //   console.log(res);
+  //   console.timeEnd("x");
+  // });
 
   //Promise all
+  //전부 완료되길 기다렸다가 결과를 출력 - 가장 느린시간 까지 기다림
   //   console.time("x");
   //   Promise.all([f1(), f2(), f3()]).then((res) => {
   //     console.log(res);
@@ -64,11 +83,12 @@ export default function Test() {
   //   });
 
   //Promise chaining
-  //   console.log("시작");
-  //   f1()
-  //     .then((res) => f2(res))
-  //     .then((res) => f3(res))
-  //     .then((res) => console.log(res))
-  //     .catch(console.log)
-  //     .finally(() => console.log("the End"));
+  //전체실행 - 차례대로 전부 하나씩 실행
+  // console.log("시작");
+  // f1()
+  //   .then((res) => f2(res))
+  //   .then((res) => f3(res))
+  //   .then((res) => console.log(res))
+  //   .catch(console.log)
+  //   .finally(() => console.log("the End"));
 }
